2a894c7fddb91fb4db64086df28478c4a72efefd
IsBabushka <mak050605@email.com>
2025-11-10 18:06:52 +0800

    Add comment (still test)

--- src/ourdocs/NewJFrame.java
+++ src/ourdocs/NewJFrame.java
@@ -17,7 +17,7 @@ public class NewJFrame extends javax.swing.JFrame {
     public NewJFrame() {
         initComponents();
         System.out.println("TESTING (by allen)");
-        
+        // what?
     }
 
     /**
